c87f3a9d1e08e4bba4822d4a561644873357f791
Zak Erving <zak@riptideio.com>
2019-07-17 17:27:53 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+// NODE ENVIRONMENT VARIABLES
+require('dotenv').config({ path: 'variables.env' })
+
+// NODE MODULES ===================================
+// ================================================
+const fs = require('fs')
+const translate = require('translate')
+      translate.key = process.env.GOOGLE_API
+
+// IMPORTS ========================================
+// ================================================
+var { phrases } = require('./src/sample')
+
+// VARIABLES ======================================
+// ================================================
+var re = new RegExp(/{[A-Za-z]+}/gi)
+
+// READY ==========================================
+// ================================================
+goTranslate('es').then(result => {
+  writeResult('./dist/', 'phrases', result)
+})
+
+// FUNCTIONS ======================================
+// ================================================
+async function goTranslate(lang) {
+  var obj = {}
+  
+  for(const p in phrases) {
+    let str = phrases[p]
+    let trx = await translate(str, lang)  
+    obj[phrases[p]] = trx
+  }
+
+  return obj
+}
+
+// UTILITIES ======================================
+// ================================================
+function writeResult(dir, filename, data) {
+  fs.writeFile(`${dir}/${filename}.json`, JSON.stringify(data, null, 2), err => {
+    if(err) throw err
+    console.log(`${filename}.json was saved into ${dir}.`)
+  })
+}
---
+++ src/sample.js
@@ -0,0 +1,5 @@
+exports.phrases = [
+  "I'd like to order a large pizza, please.",
+  "Do you have jalapeños?",
+  "Yes, extra anchovies."
+]
