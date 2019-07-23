1123846c6e9bcbc381919154551f7fe8362260aa
Zak Erving <zak@riptideio.com>
2019-07-23 17:08:36 +0200

    Better i18n object building after translation with one language

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ const translate = require('translate')
 // ================================================
 var { phrases } = require('./src/sample')
 
+src_keys = Object.keys(phrases).map(phrase => {
+  return phrase
+})
+
 // VARIABLES ======================================
 // ================================================
 var openTag_re = new RegExp(/\<[A-Za-z0-9]+([-][A-Za-z0-9]+)?\s?[A-Za-z0-9]+([-][A-Za-z0-9]+)?(.+?)\>/gi)
@@ -27,30 +31,31 @@ goTranslate('it').then(result => {
 // ================================================
 async function goTranslate(lang) {
   var obj = {}
-  var ind = 0
   
-  for(const p in phrases) {
-    let str = phrases[p]
-  
+  for(const p in src_keys) {
+    let src_key = src_keys[p]
+
+    let str = phrases[src_key]['en-us']
     let elem_openTag = str.match(openTag_re)
     let elem_closeTag = str.match(closeTag_re)
     let template_strs = str.match(templateStr_re)
     
-    // DON'T TRANSLATE HTML ELEMENTS OR VARIABLES
+    // // DON'T TRANSLATE HTML ELEMENTS OR VARIABLES
     str = elem_openTag ? subOut(elem_openTag, str, 'OPENER')      : str
     str = elem_closeTag ? subOut(elem_closeTag, str, 'CLOSER')    : str
     str = template_strs ? subOut(template_strs, str, 'TEMPLATE')  : str
     
-    // TRANSLATE DIS BITCH!
+    // // TRANSLATE DIS BITCH!
     let trx = await translate(str, lang)
     
-    // REPLACE DUMMY TEXT WITH ORIGINAL HTML ELEMENTS & VARIABLES
+    // // REPLACE DUMMY TEXT WITH ORIGINAL HTML ELEMENTS & VARIABLES
     trx = elem_openTag ? subIn(elem_openTag, trx, 'OPENER')     : trx
     trx = elem_closeTag ? subIn(elem_closeTag, trx, 'CLOSER')   : trx
     trx = template_strs ? subIn(template_strs, trx, 'TEMPLATE') : trx
 
-    obj[phrases[ind]] = trx
-    ind++
+    obj[src_key] = {}
+    obj[src_key]['en-us'] = str
+    obj[src_key][lang] = trx
   }
 
   return obj
--- src/sample.js
+++ src/sample.js
@@ -1,5 +1,11 @@
-exports.phrases = [
-  "I'd like to order a large pizza, please.",
-  "Do you have jalapeños?",
-  "Yes, extra anchovies."
-]
+exports.phrases = {
+  "orderPizza": {
+    "en-us": "I'd like to order a {large_pizza}, please."
+  },
+  "doYouHaveJalapenos": {
+    "en-us": "Do you have jalapeños?"
+  },
+  "extraAnchovies": {
+    "en-us": "Yes, please add extra anchovies."
+  }
+}
